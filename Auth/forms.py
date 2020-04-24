0fe31369a5bf1a3bde0582079aa4137cfe811fe7
shubham7298 <shubhamsingh7298@gmail.com>
2020-04-25 01:54:13 +0530

    added model and user form

---
+++ Auth/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from Auth.models import UserInfo
+
+class UserInfoForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput)
+    class Meta():
+        model = UserInfo
+        fields = ('name','email','password')
--- Auth/models.py
+++ Auth/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class UserInfo(models.Model):
+    id = models.AutoField( auto_created = True, primary_key = True)
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=254)
+    password = models.CharField(max_length=100)
