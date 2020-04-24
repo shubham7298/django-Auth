1916431a313de8f1941fdaa53d28362c78018e38
shubham7298 <shubhamsingh7298@gmail.com>
2020-04-25 02:18:32 +0530

    register user

--- Auth/urls.py
+++ Auth/urls.py
@@ -4,4 +4,6 @@ from Auth import views
 
 urlpatterns=[
     url(r'^$',views.index,name='index'),
+    url(r'^register/$',views.register,name='register'),
+    url(r'^login/$',views.login,name='login'),
 ]
--- Auth/views.py
+++ Auth/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
+from Auth.forms import UserInfoForm
 
 def index(request):
     return render(request,'index.html')
+
+def register(request):
+    registered = False
+    if request.method == 'POST':
+        profile_form = UserInfoForm(data=request.POST)
+        print(profile_form)
+        if profile_form.is_valid():
+            profile = profile_form.save(commit=False)
+            profile.save()
+            registered = True
+        else:
+            print(profile_form.errors)
+    else:
+        profile_form = UserInfoForm()
+    return render(request,'registration.html',
+                          {'profile_form':profile_form,
+                           'registered':registered})
+
+def login(request):
+    pass
