682685e6cb9ff7656d152b416f3790739ce40a39
shubham7298 <shubhamsingh7298@gmail.com>
2020-04-25 19:44:41 +0530

    build APIs

--- Auth/urls.py
+++ Auth/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
     url(r'^$',views.index,name='index'),
     url(r'^register/$',views.register,name='register'),
     url(r'^login/$',views.login,name='login'),
+    url(r'^home/$',views.home,name='home'),
 ]
--- Auth/views.py
+++ Auth/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from Auth.forms import UserInfoForm
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.hashers import make_password
 from Auth.models import UserInfo
-from django.contrib.auth import authenticate, login, logout
+import jwt,json
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 
@@ -29,17 +30,47 @@ def register(request):
                           {'profile_form':profile_form,
                            'registered':registered})
 
+@csrf_exempt
 def login(request):
     if request.method == 'POST':
         email = request.POST.get('email')
         password = make_password(request.POST.get('password'),salt)
         try:
-            print(UserInfo.objects.get(email=email,password=password))
-            return HttpResponseRedirect(reverse('index'))
+            user = UserInfo.objects.get(email=email,password=password)
 
-        except:
+        except UserInfo.DoesNotExist:
             print("Someone tried to login and failed.")
             print("They used email: {} and password: {}".format(email,password))
             return HttpResponse("Invalid login details given")
+        
+        if user:
+            payload = {
+                'id': user.id,
+                'email': user.email,
+            }
+            jwt_token = {'token': jwt.encode(payload, "SECRET_KEY").decode('utf-8')}
+            return HttpResponse(json.dumps( jwt_token),status=200,content_type="application/json")
+        else:
+            return HttpResponse(json.dumps({'Error': "Invalid credentials"}),status=400,content_type="application/json")
     else:
         return render(request, 'login.html', {})
+
+def home(request):
+    auth = request.headers.get('Authorization')
+    if not auth:
+        return None
+
+    payload = jwt.decode(auth, "SECRET_KEY")
+    email = payload['email']
+    userid = payload['id']
+    msg = {'Error': "Token mismatch",'status' :"401"}
+    try:
+        user = UserInfo.objects.get(email=email,id=userid,)
+               
+    except jwt.ExpiredSignature or jwt.DecodeError or jwt.InvalidTokenError:
+        return HttpResponse({'Error': "Token is invalid"}, status="403")
+    except User.DoesNotExist:
+        return HttpResponse({'Error': "Internal server error"}, status="500")
+
+    return HttpResponse(json.dumps({"name":user.name}),content_type="application/json")
+
